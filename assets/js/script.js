d3207fb191f20ea10104a40b2970a8ecef96bd09
salem z <salem.zerezghi2@gmail.com>
2022-04-23 13:47:11 -0400

    deleted unneeded files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,73 +1,52 @@
-// variables to select html elements
-// var startBtn = document.querySelector(".start-btn");
-// var quizContainer = document.getElementById("quiz");
+//declaing my questions and possible answers
+const quizContainer = document.getElementById("quiz");
+var myQuestions = [
+  {
+    question: "Who created Javascript",
+    multChoices: {
+      a: "adam",
+      b: "west",
+      c: "adriana",
+      d: "Brendan Eich",
+    },
+    answer: "Brendan Eich",
+  },
+  {
+    question: "What does HTML stand for",
+    multChoices: {
+      a: "Hyper Text Processor",
+      b: "Hyper Tech Multi Language ",
+      c: "Hyper Text Markup Language",
+      d: "Hyper Texture Modality Learning",
+    },
+    answer: "c",
+  },
+  {
+    question: "Which one of these is a JavaScript package manager?",
+    multChoices: {
+      a: "Node.js",
+      b: "TypeScript",
+      c: "npm",
+      d: "MVP",
+    },
+    answer: "c",
+  },
+  {
+    question: "Which tool can you use to ensure code quality?",
+    multChoices: {
+      a: "Angular",
+      b: "jQuery",
+      c: "RequireJS",
+      d: "ESLint",
+    },
+    answer: "d",
+  },
+];
 
-// create function to start quiz
+//ref to elements
+var timer = document.getElementById("timer");
+var timeLeft = document.getElementById("timeLeft");
+var timesUp = document.getElementById("timesUp");
 
-// start demo
-var index = 0
-
-function qqiz () {
-  var div = document.createElement('div')
-div.setAttribute('class','.question')
-  div.textContent = myQuestion[index].question
-  // anwA.textContent = myQuestion[index].answers.a
-  
-    if(event.target.textContent === myQuestions[index].correctAnswers) {
-      score ++
-    } else {
-      timer --
-    }
-}
-btn.addEventListener('click', ()=>{
-answer.innerHTML = ''
-qqiz()
-  index++
-})
-
-// end demo
-
-
-
-
-
-function startQuiz() {
-  // quizContainer.setAttribute("class", "activeQuiz");
-  // console.log("CLICKED")
-  document.getElementById("start-btn").addEventListener("click", startQuiz);
-
-  myQuestions.forEach((mainQuestion, multChoices) => {
-    document.createElement('div').textContent = mainQuestion.question
-
-    mainQuestion.answers.a
-
-    // for (letter in mainQuestion.answers) {
-    //   answers.push(
-    //     `<label>
-    //     <input type="radio" name="question${multChoices}" value="${letter}">
-    //     ${letter} 
-    //     ${mainQuestion.answers[letter]}
-    //   </label>`
-    //   );
-    // }
-
-    output.push(
-      `<div class="question"> ${mainQuestion.question} </div>
-    <div class="answers"> ${answers.join("")} </div>`
-    );
-
-    quizContainer.innerHTML = output.join("");
-    
-
-    // variables for js functionality
-    var timeCount = 20;
-
-    setInterval(() => {
-      timeCount--;
-      console.log(timeCount);
-      document.querySelector(".timer_sec").innerHTML = timeCount;
-    }, 1000);
-  });
-
-  console.log("testing for loop");
-}
+var startDiv = document.getElementById("start");
+var startQuizBtn = document.getElementById("start-btn");
